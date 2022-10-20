aa4886b2321044be3daea019ac78f04d53813cca
MrPotential03 <EJW76@pitt.edu>
2022-10-19 20:35:00 -0400

    Add Backend Python File

---
+++ backend_dev.py
@@ -0,0 +1,8 @@
+# We need to develop frontend javascript
+# Two inputs from the client
+  # One being the cellNumber where we are working at
+  # The other being a string of the current state of the board
+# Methods
+  # One for all the win conditions
+  # One to transfer the current state string to the 2-d array to work on
+  # 
