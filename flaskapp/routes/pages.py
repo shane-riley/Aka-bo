afa137d9d05e881ed507b138d2bc7367cf607914
Shane Riley <riley.shane.patrick@gmail.com>
2022-11-28 11:09:57 -0500

    Clean local

--- flaskapp/routes/pages.py
+++ flaskapp/routes/pages.py
@@ -1,5 +1,4 @@
 import flask
-from .__init__ import app
 from firebase_admin import auth 
 from flaskapp import Akabo
 
@@ -14,21 +13,13 @@ def setup_pages(app: Akabo):
         """
         return flask.redirect("/s/index.html", code=302)
 
-    # Login page
+    # Login/newuser page
     @app.route('/login')
     def login_page():
         """
         Login page route.
         """
         return flask.redirect("/s/login.html", code=302)
-
-    # Newuser page
-    @app.route('/newuser')
-    def newuser_page():
-        """
-        User creation page route.
-        """
-        return flask.redirect("/s/newuser.html", code=302)
         
     # Profile Page
     @app.route('/profilepage')
@@ -49,21 +40,8 @@ def setup_pages(app: Akabo):
         # TODO: Implement me
         return login_page()
 
-    # Matchmaking (drop to login if not authorized)
-    @app.route('/matchmaking/<string:game>/<string:ticket>')
-    def matchmaking_page(game, ticket):
-        """
-        User matchmaking page route.
-
-        If not logged in, drop to login page
-
-        If ticket doesn't match with the user, return a 404
-        """
-        # TODO: Implement me
-        return login_page()
-
-    # Game (drop to login if not authorized)
-    @app.route('/matchmaking/<string:game>/<string:gameid>')
+    # Game page
+    @app.route('/game')
     def game_page(game, ticket):
         """
         User game page route.
@@ -74,30 +52,3 @@ def setup_pages(app: Akabo):
         """
         # TODO: Implement me
         return login_page()
-
-# M2
-# @app.route('/email')
-# def root():
-#     return flask.redirect("/s/email-password.html", code=302)
-
-# @app.route('/send', methods=['POST', 'GET'])
-# def checkUser():
-#     # return flask.redirect("/s/WelcomePage.html", code=302)
-#     # result = flask.request.args
-#     result = flask.request.form
-
-#     id_token = result['token']
-#     # id_token = id_token['value']
-#     # id_token = id_token[2]
-#     # return flask.render_template('index.html')
-#     # id_token comes from the client app (shown above)
-#     try:
-#         auth.verify_id_token(id_token)
-#         return flask.redirect("/s/WelcomePage.html", code=302)
-#     except:
-#         return flask.redirect("/s/index.html", code=302)
-#     # return flask.redirect("/s/WelcomePage.html", code=302)
-
-# @app.route('/connect4')
-# def game():
-#     return flask.redirect("/s/connect4.html", code=302)
